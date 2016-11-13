6fd7e9af6c8129480e2ad37a1f44688c406060f2
lanxinxichen@126.com <lanxinxichen@126.com>
2016-11-13 21:48:01 +0800

    注释理解了tfidf的代码

--- jieba/analyse/tfidf.py
+++ jieba/analyse/tfidf.py
@@ -6,14 +6,14 @@ import jieba.posseg
 from operator import itemgetter
 
 _get_module_path = lambda path: os.path.normpath(os.path.join(os.getcwd(),
-                                                 os.path.dirname(__file__), path))
+                                                              os.path.dirname(__file__), path))
 _get_abs_path = jieba._get_abs_path
 
 DEFAULT_IDF = _get_module_path("idf.txt")
 
 
 class KeywordExtractor(object):
-
+    # 停用词
     STOP_WORDS = set((
         "the", "of", "is", "and", "to", "in", "that", "we", "for", "an", "are",
         "by", "be", "as", "on", "with", "can", "if", "from", "which", "you", "it",
@@ -21,6 +21,7 @@ class KeywordExtractor(object):
     ))
 
     def set_stop_words(self, stop_words_path):
+        """手动设置停用词词典"""
         abs_path = _get_abs_path(stop_words_path)
         if not os.path.isfile(abs_path):
             raise Exception("jieba: file does not exist: " + abs_path)
@@ -33,15 +34,15 @@ class KeywordExtractor(object):
 
 
 class IDFLoader(object):
-
     def __init__(self, idf_path=None):
         self.path = ""
-        self.idf_freq = {}
-        self.median_idf = 0.0
+        self.idf_freq = {}  # idf词频统计
+        self.median_idf = 0.0  # 平均词频，中位数，为了确定矩阵中心
         if idf_path:
             self.set_new_path(idf_path)
 
     def set_new_path(self, new_idf_path):
+        """手动指定tf-idf词频词典"""
         if self.path != new_idf_path:
             self.path = new_idf_path
             content = open(new_idf_path, 'rb').read().decode('utf-8')
@@ -50,17 +51,16 @@ class IDFLoader(object):
                 word, freq = line.strip().split(' ')
                 self.idf_freq[word] = float(freq)
             self.median_idf = sorted(
-                self.idf_freq.values())[len(self.idf_freq) // 2]
+                self.idf_freq.values())[len(self.idf_freq) // 2]  # 排并取出中位数
 
     def get_idf(self):
         return self.idf_freq, self.median_idf
 
 
 class TFIDF(KeywordExtractor):
-
     def __init__(self, idf_path=None):
-        self.tokenizer = jieba.dt
-        self.postokenizer = jieba.posseg.dt
+        self.tokenizer = jieba.dt  # 结巴分词器
+        self.postokenizer = jieba.posseg.dt  # 带词性的分词器
         self.stop_words = self.STOP_WORDS.copy()
         self.idf_loader = IDFLoader(idf_path or DEFAULT_IDF)
         self.idf_freq, self.median_idf = self.idf_loader.get_idf()
@@ -74,16 +74,16 @@ class TFIDF(KeywordExtractor):
 
     def extract_tags(self, sentence, topK=20, withWeight=False, allowPOS=(), withFlag=False):
         """
-        Extract keywords from sentence using TF-IDF algorithm.
-        Parameter:
-            - topK: return how many top keywords. `None` for all possible words.
-            - withWeight: if True, return a list of (word, weight);
-                          if False, return a list of words.
-            - allowPOS: the allowed POS list eg. ['ns', 'n', 'vn', 'v','nr'].
-                        if the POS of w is not in this list,it will be filtered.
-            - withFlag: only work with allowPOS is not empty.
-                        if True, return a list of pair(word, weight) like posseg.cut
-                        if False, return a list of words
+        用TF-IDF算法抽取关键词
+        参数:
+            - topK: 返回多少个关键词，None为不限制。
+            - withWeight: 如果是True, 返回一个列表，结构为：[('服务', 2.671075298535)];
+                          如果是False, 返回所有word的列表，结构为['服务', '商品']。
+            - allowPOS: 允许词性的列表. ['ns', 'n', 'vn', 'v','nr'].
+                        如果一个词的词性不在这个列表里面，那么这个词会被过滤掉。
+            - withFlag: 只有当allowPOS不为空的时候才会生效。
+                        如果是True, 返回一个列表，结构为[(pair('服务', 'vn'), 2.671075298535)]，参考posseg.cut分词器
+                        如果是False, 返回word的列表
         """
         if allowPOS:
             allowPOS = frozenset(allowPOS)
@@ -100,10 +100,12 @@ class TFIDF(KeywordExtractor):
             wc = w.word if allowPOS and withFlag else w
             if len(wc.strip()) < 2 or wc.lower() in self.stop_words:
                 continue
-            freq[w] = freq.get(w, 0.0) + 1.0
+            freq[w] = freq.get(w, 0.0) + 1.0  # 统计这句话中的词频
         total = sum(freq.values())
         for k in freq:
             kw = k.word if allowPOS and withFlag else k
+            # 这句话中的这个词语的权重 = 词频 * （该词语的权重/总词语数量）
+            # 此算法还待论证，等我比较完其他TF-IDF再添加注释
             freq[k] *= self.idf_freq.get(kw, self.median_idf) / total
 
         if withWeight:
